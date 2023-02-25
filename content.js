c4f01d406524dd9393cda7d8f6a64a9881fbcbd2
MarkH <markh@bellemoncon>
2023-02-25 19:38:00 +0100

    storing and getting song data

--- content.js
+++ content.js
@@ -2,19 +2,37 @@
 // Display it in the popup
 
 chrome.runtime.sendMessage({ action: "getSong" }, function (response) {
+  const infoSpan = document.getElementById("info");
   if (response) {
+    const song = response.song;
+    const text = `The most recent random lyric was "${song.lyric}", a line from ${song.song} which is on ${song.album}`;
     // Store the response in local storage so we can display it in the popup even
     // if the user clicks the button more than once
-    localStorage.setItem("lastSong", JSON.stringify(response.song));
-    const infoSpan = document.getElementById("info");
-    infoSpan.textContent = `The most recent random lyric was "${response.song.lyric}", a line from ${response.song.song} which is on ${resonse.song.album}`;
+    infoSpan.textContent = text;
+    // localStorage.setItem("lastSong", JSON.stringify(response.song));
+    chrome.storage.local.set({ songData: song }, function () {
+      console.log("Stored song locally");
+    });
   } else {
+    infoSpan.textContent = `There's no lyric to show right now. Check back in an hour or two.`;
     console.log("No response so far");
   }
 });
 
 // Get locally stored song when the user clicks the extension button again
-document.getElementById("button").addEventListener("click", function () {
+
+chrome.local.storage.get(["songData"], function (result) {
+  if (result.songData) {
+    const infoSpan = document.getElementById("info");
+    const text = `The most recent random lyric was "${song.lyric}", a line from ${song.song} which is on ${song.album}`;
+    infoSpan.textContent = text;
+  } else {
+    infoSpan.textContent = `There's no lyric to show right now. Check back in an hour or two.`;
+    console.log("No response so far");
+  }
+});
+
+/* document.getElementById("button").addEventListener("click", function () {
   const lastSong = JSON.parse(localStorage.getItem("lastSong"));
 
   if (lastSong) {
@@ -23,4 +41,4 @@ document.getElementById("button").addEventListener("click", function () {
   } else {
     console.log("No last song stored");
   }
-});
+}); */

8b2480afd41bf6863b68641061adbbd702e5b770
MarkH <markh@bellemoncon>
2023-03-19 23:47:35 +0100

    content.js - added songData interface to type the returned object from local storage

--- src/content.ts
+++ src/content.ts
@@ -11,75 +11,85 @@ const albumCoverImg = <HTMLImageElement>(
 const cover = <HTMLDivElement>document.getElementById("cover");
 const noResultText: string = `There's no lyric to show right now. Check back in an hour or two.`;
 
+interface songData {
+  lyric: string;
+  song_name: string;
+  album: string;
+  albumId: number;
+}
+
 // Get locally stored song when the user clicks the extension button
 
-chrome.storage.local.get("songData", function (result) {
-  if (result.songData) {
-    lyricSpan.textContent = `${result.songData.lyric}`;
-    songSpan.textContent = `Song: ${result.songData.song_name}`;
-    albumSpan.textContent = `Album: ${result.songData.album}`;
-    const albumId: number = result.songData.albumId;
-    // get the album's cover art
-    switch (albumId) {
-      case 1:
-        albumCoverImg.src = chrome.runtime.getURL(
-          "/img/covers/album_cover_1.jpg"
-        );
-        albumCoverImg.alt = "Can't Buy A Thrill cover art";
-        break;
-      case 2:
-        //albumCoverImg.src = "/img/covers/album_cover_2.jpg";
-        albumCoverImg.src = chrome.runtime.getURL(
-          "/img/covers/album_cover_2.jpg"
-        );
-        albumCoverImg.alt = "Countdown To Ecstasy cover art";
-        break;
-      case 3:
-        albumCoverImg.src = chrome.runtime.getURL(
-          "/img/covers/album_cover_3.jpg"
-        );
-        albumCoverImg.alt = chrome.runtime.getURL("Pretzel Logic cover art");
-        break;
-      case 4:
-        albumCoverImg.src = chrome.runtime.getURL(
-          "/img/covers/album_cover_4.jpg"
-        );
-        albumCoverImg.alt = "Katy Lied cover art";
-        break;
-      case 5:
-        albumCoverImg.src = chrome.runtime.getURL(
-          "/img/covers/album_cover_5.jpg"
-        );
-        albumCoverImg.alt = "The Royal Scam cover art";
-        break;
-      case 6:
-        albumCoverImg.src = chrome.runtime.getURL(
-          "/img/covers/album_cover_6.jpg"
-        );
-        albumCoverImg.alt = "Aja cover art";
-        break;
-      case 7:
-        albumCoverImg.src = chrome.runtime.getURL(
-          "/img/covers/album_cover_7.jpg"
-        );
-        albumCoverImg.alt = "Gaucho cover art";
-        break;
-      case 8:
-        albumCoverImg.src = chrome.runtime.getURL(
-          "https://media.graphassets.com/DdHAZfEwQqW0gUUKIC7f"
-        );
-        break;
-      case 9:
-        albumCoverImg.src = chrome.runtime.getURL(
-          "https://media.graphassets.com/Uk751ZwLTSk0PCjSZr7M"
-        );
-        break;
-      default:
-        albumCoverImg.src = chrome.runtime.getURL(
-          "/img/covers/album_cover_1.jpg"
-        );
+chrome.storage.local.get(
+  "songData",
+  function (result: { songData?: songData }) {
+    if (result.songData) {
+      lyricSpan.textContent = `${result.songData.lyric}`;
+      songSpan.textContent = `Song: ${result.songData.song_name}`;
+      albumSpan.textContent = `Album: ${result.songData.album}`;
+      const albumId: number = result.songData.albumId;
+      // get the album's cover art
+      switch (albumId) {
+        case 1:
+          albumCoverImg.src = chrome.runtime.getURL(
+            "/img/covers/album_cover_1.jpg"
+          );
+          albumCoverImg.alt = "Can't Buy A Thrill cover art";
+          break;
+        case 2:
+          //albumCoverImg.src = "/img/covers/album_cover_2.jpg";
+          albumCoverImg.src = chrome.runtime.getURL(
+            "/img/covers/album_cover_2.jpg"
+          );
+          albumCoverImg.alt = "Countdown To Ecstasy cover art";
+          break;
+        case 3:
+          albumCoverImg.src = chrome.runtime.getURL(
+            "/img/covers/album_cover_3.jpg"
+          );
+          albumCoverImg.alt = chrome.runtime.getURL("Pretzel Logic cover art");
+          break;
+        case 4:
+          albumCoverImg.src = chrome.runtime.getURL(
+            "/img/covers/album_cover_4.jpg"
+          );
+          albumCoverImg.alt = "Katy Lied cover art";
+          break;
+        case 5:
+          albumCoverImg.src = chrome.runtime.getURL(
+            "/img/covers/album_cover_5.jpg"
+          );
+          albumCoverImg.alt = "The Royal Scam cover art";
+          break;
+        case 6:
+          albumCoverImg.src = chrome.runtime.getURL(
+            "/img/covers/album_cover_6.jpg"
+          );
+          albumCoverImg.alt = "Aja cover art";
+          break;
+        case 7:
+          albumCoverImg.src = chrome.runtime.getURL(
+            "/img/covers/album_cover_7.jpg"
+          );
+          albumCoverImg.alt = "Gaucho cover art";
+          break;
+        case 8:
+          albumCoverImg.src = chrome.runtime.getURL(
+            "https://media.graphassets.com/DdHAZfEwQqW0gUUKIC7f"
+          );
+          break;
+        case 9:
+          albumCoverImg.src = chrome.runtime.getURL(
+            "https://media.graphassets.com/Uk751ZwLTSk0PCjSZr7M"
+          );
+          break;
+        default:
+          albumCoverImg.src = chrome.runtime.getURL(
+            "/img/covers/album_cover_1.jpg"
+          );
+      }
+    } else {
+      lyricSpan.textContent = "Lyric goes here.";
     }
-  } else {
-    lyricSpan.textContent = "Lyric goes here.";
   }
-});
+);

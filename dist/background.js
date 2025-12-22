25579a30bdc5b28b5ebefd1e8a3ea1bd8f288645
headexpanded <mhatherly@mixvoip.com>
2025-12-22 14:41:34 +0100

    1.6.0: update server address

--- dist/background.js
+++ dist/background.js
@@ -33,7 +33,7 @@ async function getSong() {
     const elapsedTime = currentTime - lastFetchTime;
     if (elapsedTime >= FETCH_INTERVAL || lastFetchTime === 0) {
         try {
-            const apiUrl = "http://45.158.140.32/random-lyric";
+            const apiUrl = "http://159.65.112.232/random-lyric";
             const response = await fetch(apiUrl, {
                 method: "GET",
                 headers: {
